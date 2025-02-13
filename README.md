2ad608e2a191785cb0fa0d414eb704797550e814
Arthur Lobo <arthur.lobo@analitica.ag>
2025-02-13 16:00:47 -0300

    feat: starting lib

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lib Relatório Gerencial Agrícola
---
+++ src/interfaces/telemetry.ts
@@ -0,0 +1,6 @@
+export interface Telemetry {
+    occurrence: number;
+    sensor_name: string;
+    current_value: string;
+    equipment_code: string;
+}
